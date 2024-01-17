7b40a43433fb834cb5e499e39bc91b38eabbbfc4
Michael Wood <woodjmichael@gmail.com>
2024-01-17 18:39:42 +0100

    random search

--- PROPHET_LOAD_LSTM/main/lstm_forecaster_training.py
+++ PROPHET_LOAD_LSTM/main/lstm_forecaster_training.py
@@ -11,6 +11,7 @@ import argparse
 import configparser
 from datetime import datetime, timedelta
 from pathlib import Path
+from random import shuffle
 
 if MICROGRID_PC:
     from PROPHET_DB import mysql
@@ -24,7 +25,7 @@ if MICROGRID_PC:
 # LOGGER = daiquiri.getLogger(__name__)
 
 
-def main(argv=None):
+def main(argv=None,units=None,n_back=None,n_dense=None,dropout=None):
     # read configuration file
     args = util.parse_arguments(argv)
 
@@ -59,35 +60,15 @@ def main(argv=None):
                  'Dropout_rate': config.getfloat("model_opt", "Dropout_rate"),
                  'input_dim': (data_opt['n_back'], data_opt['n_features']), 'dense_out': data_opt['n_timesteps']
                  }
-    # logger_config = {'mailhost': (config["logger"]["mailhost"], config.getint("logger", "port")),
-    #                  'fromaddr': config["logger"]["fromaddr"],
-    #                  'toaddr': config["logger"]["toaddrs"],
-    #                  'subject': config["logger"]["subject"],
-    #                  'credentials': (config["logger"]["username"], config["logger"]["password"]),
-    #                  'mail_handler': config.getboolean("logger", "mail_handler"),
-    #                  'backup_count': config.getint("logger", "backup_count")}
-
-    #
-    # talktoSQL = mysql.MySQLConnector(database=config["mysql"]["database"],
-    #                                  host=config["mysql"]["host"],
-    #                                  port=config["mysql"]["port"],
-    #                                  user=config["mysql"]["user"],
-    #                                  password=config["mysql"]["password"])
-
-    # (talktoSQL, table_mysql, column_mysql, time_column)
-    # ev_query = util.create_query(talktoSQL, config["tables"]["measures"], config["columns"]["measures"], config["tables"]["time_column"])
-    # ev_query = ev_query + 'order by TimeCol desc limit 1000'
-    # df = talktoSQL.read_query(ev_query, {config["tables"]["time_column"]})
-    # df[config["tables"]["time_column"]] = pd.to_datetime(df[config["tables"]["time_column"]], format='%Y-%m-%d %H:%M:%S%z')#.dt.tz_convert('America/Los_Angeles')
-    #
-    # df.rename(columns={config["columns"]["measures"]: 'power'}, inplace=True)
-    # df.set_index(config["tables"]["time_column"], inplace=True)
-
-    #df.to_csv('C:\\Users\\PROPHET\\Desktop\\Mike\\test.csv')
-
-    #df = pd.read_csv("C:\\Users\\PROPHET\\Documents\\Mike\\train_JPL_5_mjw.csv", sep=",")
-    #df[config["tables"]["time_column"]] = pd.to_datetime(df['timestamp_utc'], format='%d/%m/%Y %H:%M')
-    #df.set_index(config["tables"]["time_column"], inplace=True)
+    
+    if units is not None:
+        model_opt['LSTM_num_hidden_units'] = units
+    if n_back is not None:
+        data_opt['n_back'] = n_back
+    if n_dense is not None:
+        model_opt['Dense_input_dim'] = n_dense
+    if dropout is not None:
+        model_opt['Dropout_rate'] = dropout
     
     df = pd.read_csv(data_opt['data_train_path'],
                      index_col=0,
@@ -119,107 +100,10 @@ def main(argv=None):
     model, history = create_model_attention(model_opt, train_X, train_y)
     model.save(model_opt['model_path'] / Path("model.h5"))
 
-
-def grid_search(argv=None):
-    for units in [24*x for x in [1,2,3,4,5,8,10,12,14,16,18,20]]:
-        for n_back in [24*x for x in [1,2,3,4,5,8,10,12,14,16,18,20]]:
-            print(f'\n\n\n ///// units = {units} n_back = {n_back} /////\n')
-            # read configuration file
-            args = util.parse_arguments(argv)
-
-            config = util.read_config(args.config)
-            data_opt = {
-                'n_back': n_back,  # 4*24*7
-                'n_timesteps': config.getint("data_opt", "n_timesteps"),  # 4*4
-                'lag': config.getint("data_opt", "lag"),
-                'tr_per': config.getfloat("data_opt", "tr_per"),
-                'out_col': config.get("data_opt", "out_col").split(','),
-                'features': config.get("data_opt", "features").split(','),
-                'freq': config.getint("data_opt", "freq"),
-                'tr_days_step': config.getint("data_opt", "tr_days_step"),
-                'data_path': Path(config.get("data_opt", "data_path")),
-                'out_dir': Path(config.get("data_opt", "out_dir")),
-                'data_train_path':Path(config.get("data_opt", "data_path")) / Path(config.get('data_opt', 'train_csv')),
-            }
-            if data_opt['features'] == ['']:
-                data_opt['columns'] = data_opt['out_col']
-            else:
-                data_opt['columns'] = data_opt['features'] + data_opt['out_col']
-            data_opt['n_features'] = len(data_opt['columns'])
-
-            model_opt = {'Dense_input_dim': config.getint("model_opt", "Dense_input_dim"),
-                        'LSTM_num_hidden_units': units,
-                        'LSTM_layers': config.getint("model_opt", "LSTM_layers"),
-                        'metrics': config.get("model_opt", "metrics"), 'optimizer': config.get("model_opt", "optimizer"),
-                        'patience': config.getint("model_opt", "patience"),
-                        'epochs': config.getint("model_opt", "epochs"),
-                        'validation_split': config.getfloat("model_opt", "validation_split"),
-                        'model_path': Path(config.get("model_opt", "model_path")),
-                        'Dropout_rate': config.getfloat("model_opt", "Dropout_rate"),
-                        'input_dim': (data_opt['n_back'], data_opt['n_features']), 'dense_out': data_opt['n_timesteps']
-                        }
-            # logger_config = {'mailhost': (config["logger"]["mailhost"], config.getint("logger", "port")),
-            #                  'fromaddr': config["logger"]["fromaddr"],
-            #                  'toaddr': config["logger"]["toaddrs"],
-            #                  'subject': config["logger"]["subject"],
-            #                  'credentials': (config["logger"]["username"], config["logger"]["password"]),
-            #                  'mail_handler': config.getboolean("logger", "mail_handler"),
-            #                  'backup_count': config.getint("logger", "backup_count")}
-
-            #
-            # talktoSQL = mysql.MySQLConnector(database=config["mysql"]["database"],
-            #                                  host=config["mysql"]["host"],
-            #                                  port=config["mysql"]["port"],
-            #                                  user=config["mysql"]["user"],
-            #                                  password=config["mysql"]["password"])
-
-            # (talktoSQL, table_mysql, column_mysql, time_column)
-            # ev_query = util.create_query(talktoSQL, config["tables"]["measures"], config["columns"]["measures"], config["tables"]["time_column"])
-            # ev_query = ev_query + 'order by TimeCol desc limit 1000'
-            # df = talktoSQL.read_query(ev_query, {config["tables"]["time_column"]})
-            # df[config["tables"]["time_column"]] = pd.to_datetime(df[config["tables"]["time_column"]], format='%Y-%m-%d %H:%M:%S%z')#.dt.tz_convert('America/Los_Angeles')
-            #
-            # df.rename(columns={config["columns"]["measures"]: 'power'}, inplace=True)
-            # df.set_index(config["tables"]["time_column"], inplace=True)
-
-            #df.to_csv('C:\\Users\\PROPHET\\Desktop\\Mike\\test.csv')
-
-            #df = pd.read_csv("C:\\Users\\PROPHET\\Documents\\Mike\\train_JPL_5_mjw.csv", sep=",")
-            #df[config["tables"]["time_column"]] = pd.to_datetime(df['timestamp_utc'], format='%d/%m/%Y %H:%M')
-            #df.set_index(config["tables"]["time_column"], inplace=True)
-            
-            df = pd.read_csv(data_opt['data_train_path'],
-                            index_col=0,
-                            comment='#',
-                            parse_dates=True)
-            df.index = df.index.tz_localize('UTC')
-
-            df['year'] = df.index.year
-            df['month'] = df.index.month
-            df['day'] = df.index.dayofweek
-            df['hour'] = df.index.hour
-            df['minute'] = df.index.minute
-            df = df[data_opt['columns']]
-
-            ## definizione tempi inizio
-            now = datetime.utcnow()
-            fine_tr = pd.to_datetime(now).tz_localize('UTC')  ## METTERE A POSTO MA SECONDARIO
-
-            mask_tr = (df.index < fine_tr)
-            train = df.loc[mask_tr]
-
-            #### addestra il modello
-            train_X, train_y, scaler_X, scaler_y = prepare_data_train(train, data_opt)
-            train_y = train_y.reshape((train_y.shape[0], train_y.shape[1], 1))
-
-            dump(scaler_X, open(model_opt["model_path"] / Path("scaler_in.pkl"), 'wb'))
-            dump(scaler_y, open(model_opt["model_path"] / Path("scaler_out.pkl"), 'wb'))
-
-            model, history = create_model_attention(model_opt, train_X, train_y)
-            #model.save(model_opt['model_path'] / Path("model.h5"))
-
-            val_loss = min(history.history['val_loss'])
-            model.save(model_opt['model_path'] / Path(f"model_units{units}_nback{n_back}_vloss{val_loss:.4f}.h5"))
+    val_loss = min(history.history['val_loss'])
+    #model.save(model_opt['model_path'] / Path(f"model_u{units}_nb{n_back}_nd{n_dense}_d{dropout}_vl{val_loss:.4f}.h5"))
+    
+    return val_loss
         
 
 if __name__ == "__main__":
--- run_offline.py
+++ run_offline.py
@@ -1,31 +1,40 @@
-import os
 from datetime import datetime as dt
 from datetime import timedelta as td
+from random import shuffle
 import pandas as pd
 from PROPHET_LOAD_LSTM.main import lstm_forecaster_training, lstm_forecaster
-
-# def multi_test(t = datetime(2020,1,1,0,0,0),t_final=datetime(2020,1,1,1,0,0)):
-#     r = pd.DataFrame({'t':[],'rmse persist':[],'rmse lstm':[],'skill':[]})
-#     results_path = os.path.dirname(ini_path)+'/data/test_results.csv'
-#     forecast_df = pd.DataFrame({'timestamp_forecast_update':[],'predicted_activepower_ev_1':[],'persist':[]})
-#     while t < t_final:
-#         rmse_persist, rmse_lstm, skill, _forecast_df = lstm_forecaster.main([ini_path],pd.to_datetime(t),plots=False)
-#         r.loc[len(r)] = [t,rmse_persist,rmse_lstm,skill]
-#         forecast_df = pd.concat([forecast_df,_forecast_df],axis=0)
-#         r.to_csv(results_path)
-#         t += timedelta(hours=1)
-#     forecast_df.to_csv(ini_path[:-19]+'/data/output/all_forecast_df.csv')
 
 #ini_path = "/home/mjw/Code/PROPHET-Load_LSTM/lstm_forecaster.ini"
 ini_path = "C:/Users/Admin/Code/PROPHET-Load_LSTM/lstm_forecaster.ini"
 
 #lstm_forecaster_training.main([ini_path])
-#lstm_forecaster_training.grid_search([ini_path])
-
-
 #mae_persist, mae_lstm, skill, forecast = lstm_forecaster.main([ini_path],dt(2020,1,1,0,0),plots=False)
 
-mae = pd.DataFrame({'t':[],'persist':[],'lstm':[],'skill':[]})
+#
+# Random search
+#
+search_space = []
+results = pd.DataFrame({'units':[],'n_back':[],'n_dense':[],'dropout':[],'vloss':[]})
+for units in [24*x for x in [1,2,3,4,5,8,10,12,14,16,18,20]]:
+    for n_back in [24*x for x in [1,2,3,4,5,8,10,12,14,16,18,20]]:
+        for n_dense in [24]:
+            for dropout in [0,0.1,0.2]:
+                search_space.append((units,n_back,n_dense,dropout))           
+shuffle(search_space)
+for u,nb,nd,d in search_space:
+    print(f'\n\n\n ///// units={u} n_back={nb} n_dense={nd} dropout={d}/////\n')
+    try:
+        vloss = lstm_forecaster_training.main([ini_path],u,nb,nd,d)
+    except:
+        print('//////////////////// FAIL /////////////////////')
+        vloss=pd.NA
+    results.loc[len(results)] = [u,nb,nd,d,vloss]
+    results.to_csv(ini_path[:-19]+'data/output/random_search.csv')
+
+#
+# Test
+#
+"""mae = pd.DataFrame({'t':[],'persist':[],'lstm':[],'skill':[]})
 forecasts = pd.DataFrame({'timestamp_forecast_update':[],'predicted_activepower_ev_1':[],'persist':[]})
 for t in pd.date_range(dt(2019,12,5,0,0,0),dt(2020,1,15,0,0),freq='15min'):
     t=pd.to_datetime(t)
@@ -36,4 +45,4 @@ for t in pd.date_range(dt(2019,12,5,0,0,0),dt(2020,1,15,0,0),freq='15min'):
     forecasts.to_csv(ini_path[:-19]+'data/output/forecasts.csv')
     mae.loc[len(mae)] = [t,mae_persist,mae_lstm,skill]
     mae.to_csv(ini_path[:-19]+'data/output/errors.csv')
-    pass
+    pass"""
